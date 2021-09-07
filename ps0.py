f0b3e4c1bb631ee887bd4decf754d5488c9493ef
Navinder Kour <kournavinder0@gmail.com>
2021-09-08 00:56:52 +0530

    feat:ProblemSet#0

---
+++ ps0.py
@@ -0,0 +1,11 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+#Navinder kour
+#Problem set 0
+#time spent: 4 min
+
+
+    date= input("date")
+    name= input("last name")
+    print(name,date)
